9c7c24b8988bd4d0b4b50e821167a79cc62c501a
IbxDev <i.doukoure21@gmail.com>
2019-11-19 23:19:14 +0000

    almost done"

--- js/script.js
+++ js/script.js
@@ -9,6 +9,25 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+
+
 $("#shoot").click(function() {
-    $("").text($("").val());
+    $("#userChoice").text($("#input").val());
+     var cpu  = Math.random()   
+    if (cpu > 0 && cpu < 0.333) {
+        $("#computerChoice").html("rock");
+    } else if (cpu > 0.333 && cpu < 0.666) {
+        $("#computerChoice").html("paper");
+    } else if (cpu > 0.666 && cpu < 0.999) {
+        $("#computerChoice").html("scissors");
+    }
+
+    
+    var resultUser = $("#userChoice").text($("#input").val());
+    console.log(cpu);
+    if (resultUser === "rock" && "#computerChoice" === "scissors") {
+        $("#result").html("You've won!");
+    }
+    $("#computerChoice").text($(cpu).val());
 });
+
